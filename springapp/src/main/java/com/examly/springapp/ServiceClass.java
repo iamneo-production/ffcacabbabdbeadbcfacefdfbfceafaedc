5dfe457e7fd4de4482c37f01832ede6a0fb5464e
Iamneo <techadmin@iamneo.ai>
2023-08-30 14:52:57 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/ServiceClass.java
+++ springapp/src/main/java/com/examly/springapp/ServiceClass.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 
 @Service
-public class ServiceClass{
+class ServiceClass{
     @Autowired
     Respository rep;
     public ModelTask addTask(ModelTask mv)
